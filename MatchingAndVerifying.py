87428db55f2409ec875265e717f87c478de25e10
costasp11 <papanicolaoucp@gmail.com>
2026-01-30 17:22:48 -0500

    initial commit - gale shapley algorithm setup

---
+++ MatchingAndVerifying.py
@@ -0,0 +1,11 @@
+# implementation: handle edge cases such as empty files and one hospital / one student
+# also check if input is valid (student = hospital count)
+
+
+def GaleShapley():
+    return
+
+
+
+if __name__ == "__main__":
+    GaleShapley()
